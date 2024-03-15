2aca653a5586f4089b6e68c6adf24a1aad1c0409
mxrazzz <meraz.hossain@city.ac.uk>
2024-03-15 23:57:14 +0000

    changes before redux

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const RegisterPage = () => {
   const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
     email: "",
     password: "",
     confirmPassword: "",
     terms: false,
   });
+
   const [errors, setErrors] = useState({});
   const [isSubmitting, setIsSubmitting] = useState(false);
 
@@ -21,6 +25,13 @@ const RegisterPage = () => {
     let formErrors = {};
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
+    if (!formData.firstName.trim()) {
+      formErrors.firstName = "First name is required";
+    }
+
+    if (!formData.lastName.trim()) {
+      formErrors.lastName = "Last name is required";
+    }
     if (!formData.email) {
       formErrors.email = "Email is required";
     } else if (!regex.test(formData.email)) {
@@ -45,20 +56,47 @@ const RegisterPage = () => {
 
     return formErrors;
   };
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const validationErrors = validateForm();
     setErrors(validationErrors);
-    setIsSubmitting(true);
 
-    if (Object.keys(validationErrors).length === 0 && isSubmitting) {
-      console.log("Form data submitted:", formData);
-      // Proceed with API call
-      // Reset form or redirect user after successful submission
+    if (Object.keys(validationErrors).length === 0 && !isSubmitting) {
+      setIsSubmitting(true);
+
+      try {
+        await axios
+          .post("http://localhost:5000/api/auth/register", {
+            firstName: formData.firstName,
+            lastName: formData.lastName,
+            email: formData.email,
+            password: formData.password,
+            // Add more fields as needed
+          })
+          .then((response) => {
+            // Redirect to homepage after successful registration
+            navigate("/");
+          })
+          .catch((error) => {
+            if (error.response && error.response.status === 400) {
+              // Set an error state and display it in your UI
+              setErrors({ ...errors, email: error.response.data.message });
+            }
+          });
+
+        // Handle success (e.g., redirect to login page)
+        setIsSubmitting(false);
+      } catch (error) {
+        console.error("Registration error:", error.response.data);
+        setIsSubmitting(false);
+        // Handle errors (e.g., show error message)
+      }
     }
   };
-  const navigate = useNavigate();
+
+  // Inside your RegisterPage component
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#FFF7E0]">
@@ -87,98 +125,152 @@ const RegisterPage = () => {
           <p className="px-3 dark:text-gray-400">OR</p>
           <hr className="w-full dark:text-gray-400" />
         </div>
-        <form noValidate onSubmit={handleSubmit} className="space-y-8">
-          <div className="space-y-4">
-            <div className="space-y-2">
-              <label htmlFor="email" className="block text-sm text-[#5C3D2E]">
-                Email address
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="email@example.com"
-                className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-[#FFF7E0] dark:text-[#5C3D2E] focus:dark:border-violet-400"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-              {errors.email && (
-                <p className="text-red-500 text-xs">{errors.email}</p>
-              )}
-            </div>
-            <div className="space-y-2">
-              <label
-                htmlFor="password"
-                className="block text-sm text-[#5C3D2E]"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="Password"
-                className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-[#FFF7E0] dark:text-[#5C3D2E] focus:dark:border-violet-400"
-                value={formData.password}
-                onChange={handleChange}
-                required
-              />
-              {errors.password && (
-                <p className="text-red-500 text-xs">{errors.password}</p>
-              )}
-            </div>
-            <div className="space-y-2">
-              <label
-                htmlFor="confirmPassword"
-                className="block text-sm text-[#5C3D2E]"
-              >
-                Confirm Password
-              </label>
-              <input
-                type="password"
-                name="confirmPassword"
-                id="confirmPassword"
-                placeholder="Confirm Password"
-                className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-[#FFF7E0] dark:text-[#5C3D2E] focus:dark:border-violet-400"
-                value={formData.confirmPassword}
-                onChange={handleChange}
-                required
-              />
-              {errors.confirmPassword && (
-                <p className="text-red-500 text-xs">{errors.confirmPassword}</p>
-              )}
-            </div>
-            <div className="flex items-start">
-              <input
-                type="checkbox"
-                id="terms"
-                name="terms"
-                checked={formData.terms}
-                onChange={handleChange}
-                className="w-4 h-4 mt-1 border rounded dark:border-gray-700 focus:ring-violet-400"
-              />
-              <label htmlFor="terms" className="ml-2 text-sm text-[#5C3D2E]">
-                I agree to the{" "}
-                <span
-                  className="underline cursor-pointer"
-                  onClick={() => navigate("/terms-and-conditions")}
-                >
-                  Terms and Conditions
-                </span>
-              </label>
-              {errors.terms && (
-                <p className="text-red-500 text-xs">{errors.terms}</p>
-              )}
-            </div>
+        {isSubmitting ? (
+          <div className="flex justify-center items-center">
+            <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-golden"></div>
           </div>
-          <button
-            type="submit"
-            className="w-full px-8 py-3 font-semibold rounded-md bg-[#D4AF37] text-[#1A365D]"
-          >
-            Register
-          </button>
-        </form>
+        ) : (
+          <form noValidate onSubmit={handleSubmit} className="space-y-8">
+            <div className="space-y-4">
+              {/* First Name Field */}
+              <div className="space-y-2">
+                <label
+                  htmlFor="firstName"
+                  className="block text-sm text-[#5C3D2E]"
+                >
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  name="firstName"
+                  id="firstName"
+                  placeholder="First Name"
+                  className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-[#FFF7E0] dark:text-[#5C3D2E] focus:dark:border-violet-400"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                  required
+                />
+                {errors.firstName && (
+                  <p className="text-red-500 text-xs">{errors.firstName}</p>
+                )}
+              </div>
+
+              {/* Last Name Field */}
+              <div className="space-y-2">
+                <label
+                  htmlFor="lastName"
+                  className="block text-sm text-[#5C3D2E]"
+                >
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  name="lastName"
+                  id="lastName"
+                  placeholder="Last Name"
+                  className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-[#FFF7E0] dark:text-[#5C3D2E] focus:dark:border-violet-400"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                  required
+                />
+                {errors.lastName && (
+                  <p className="text-red-500 text-xs">{errors.lastName}</p>
+                )}
+              </div>
+
+              <div className="space-y-2">
+                <label htmlFor="email" className="block text-sm text-[#5C3D2E]">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="email@example.com"
+                  className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-[#FFF7E0] dark:text-[#5C3D2E] focus:dark:border-violet-400"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+                {errors.email && (
+                  <p className="text-red-500 text-xs">{errors.email}</p>
+                )}
+              </div>
+              <div className="space-y-2">
+                <label
+                  htmlFor="password"
+                  className="block text-sm text-[#5C3D2E]"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                  className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-[#FFF7E0] dark:text-[#5C3D2E] focus:dark:border-violet-400"
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                />
+                {errors.password && (
+                  <p className="text-red-500 text-xs">{errors.password}</p>
+                )}
+              </div>
+              <div className="space-y-2">
+                <label
+                  htmlFor="confirmPassword"
+                  className="block text-sm text-[#5C3D2E]"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  id="confirmPassword"
+                  placeholder="Confirm Password"
+                  className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-[#FFF7E0] dark:text-[#5C3D2E] focus:dark:border-violet-400"
+                  value={formData.confirmPassword}
+                  onChange={handleChange}
+                  required
+                />
+                {errors.confirmPassword && (
+                  <p className="text-red-500 text-xs">
+                    {errors.confirmPassword}
+                  </p>
+                )}
+              </div>
+              <div className="flex items-start">
+                <input
+                  type="checkbox"
+                  id="terms"
+                  name="terms"
+                  checked={formData.terms}
+                  onChange={handleChange}
+                  className="w-4 h-4 mt-1 border rounded dark:border-gray-700 focus:ring-violet-400"
+                />
+                <label htmlFor="terms" className="ml-2 text-sm text-[#5C3D2E]">
+                  I agree to the{" "}
+                  <span
+                    className="underline cursor-pointer"
+                    onClick={() => navigate("/terms-and-conditions")}
+                  >
+                    Terms and Conditions
+                  </span>
+                </label>
+                {errors.terms && (
+                  <p className="text-red-500 text-xs">{errors.terms}</p>
+                )}
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="w-full px-8 py-3 font-semibold rounded-md bg-[#D4AF37] text-[#1A365D]"
+            >
+              Register
+            </button>
+          </form>
+        )}
         <p className="mt-4 text-sm text-center text-[#5C3D2E]">
           Already have an account?{" "}
           <Link
